1eeb0d4d4d5a2a2757100666c129dffa0402c427
yesid10 <114708064+yesid10@users.noreply.github.com>
2026-03-09 22:37:39 +0000

    Se implementa los filtros por experiencia

--- 01-javascript/filters.js
+++ 01-javascript/filters.js
@@ -1,19 +1,21 @@
-import {state} from './config.js';
+import { state } from './config.js';
 
 state.count++;
 
 console.log(state);
 
 const filter = document.querySelector("#filter-location");
+const filterExperienceLevel = document.querySelector("#filter-experience-level");
 const mensaje = document.querySelector("#filter-selected-value");
 
-filter.addEventListener("change", () =>{
-    const jobs = document.querySelectorAll(".job-listing-card");
 
+
+filter.addEventListener("change", () => {
+    const jobs = document.querySelectorAll(".job-listing-card");
     const selectedValue = filter.value;
     console.log(selectedValue);
 
-    if(selectedValue) {
+    if (selectedValue) {
         mensaje.textContent = `Has seleccionado: ${selectedValue}`;
         mensaje.classList.add("is-selected");
     } else {
@@ -26,5 +28,27 @@ filter.addEventListener("change", () =>{
         job.classList.toggle("is-hidden", isShown === false);
     })
 
+
+})
+
+filterExperienceLevel.addEventListener("change", () => {
+
+    const jobs = document.querySelectorAll(".job-listing-card");
+
+    const selectedValueFilterExperience = filterExperienceLevel.value;
+    console.log("Filtro de experiencia: ", selectedValueFilterExperience);
+
+    if (selectedValueFilterExperience) {
+        mensaje.textContent = `Has seleccionado: ${selectedValueFilterExperience}`;
+        mensaje.classList.add("is-selected");
+    } else {
+        mensaje.textContent = "";
+    }
+
+    jobs.forEach(job => {
+        const experiencia = job.dataset.nivel;
+        const isShown =selectedValueFilterExperience === "" || selectedValueFilterExperience === experiencia;
+        job.classList.toggle("is-hidden", isShown === false);
+    })
 
 })
